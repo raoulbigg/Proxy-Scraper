075ae9b351d47fa676e2dfe1a8370b4bd230de0a
raoulbigg <raoulbiggelaar@gmail.com>
2016-08-28 12:18:19 +0200

    scrape.py

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape():
+    open('proxies2.txt', 'w')
+    url = "http://ipaddress.com/proxy-list/"
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text)
+    f1 = soup.findAll('table', attrs={'class':'table table-striped'})
+    ipPattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:[0-9]+')
+    for line in f1:
+        proxies = re.findall(ipPattern,line.text)
+        for proxy in proxies:
+            f = open('proxies2.txt', 'a')
+            f.write(proxy+'\n')
+            f.close()
+scrape()
+
